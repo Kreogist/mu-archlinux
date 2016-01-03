fbd050ff64239e3af63f8cf6c8b772bc9a4a7363
frantic1048 <archer@frantic1048.com>
2016-01-03 23:41:18 +0800

    fix wrong pushing URL...

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,8 +4,7 @@ export PKGREL=$(<PKGBUILD grep pkgrel= | cut -d"=" -f 2 | sed "s/[\"']//g")
 
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
-git diff HEAD
 git add .
 git commit -m "Travis: updated to ${PKGVER}-${PKGREL}"
 git tag "$PKGVER.$PKGREL"
-git push --quiet https://$GH_TOKEN@github.com/frantic1048/Mu.git --follow-tags
+git push --quiet https://$GH_TOKEN@github.com/frantic1048/mu-archlinux.git --follow-tags
