0128af5bec4aeb9b4c3e9416c8843262c5d22e71
frantic1048 <archer@frantic1048.com>
2016-01-03 23:38:29 +0800

    fix invalid tag naming

--- before_deploy.sh
+++ before_deploy.sh
@@ -7,5 +7,5 @@ git config --global user.name $GIT_NAME
 git diff HEAD
 git add .
 git commit -m "Travis: updated to ${PKGVER}-${PKGREL}"
-git tag "$PKGVER-$PKGREL"
+git tag "$PKGVER.$PKGREL"
 git push --quiet https://$GH_TOKEN@github.com/frantic1048/Mu.git --follow-tags
