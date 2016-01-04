bba1371d84e59f0b2895fb9206f378d175511e07
frantic1048 <archer@frantic1048.com>
2016-01-04 21:56:48 +0800

    set git push.default

--- before_deploy.sh
+++ before_deploy.sh
@@ -4,10 +4,10 @@ export PKGREL=$(<PKGBUILD grep pkgrel= | cut -d"=" -f 2 | sed "s/[\"']//g")
 
 git config --global user.email $GIT_EMAIL
 git config --global user.name $GIT_NAME
-# git config --global push.default matching
+git config --global push.default simple
 git config --global push.followTags true
 git add .
-git commit -m "Travis: updated to ${PKGVER}-${PKGREL}"
+git commit -m "Travis: updated to ${PKGVER}:${PKGREL}"
 git tag "$PKGVER.$PKGREL"
 git branch
 git tag
