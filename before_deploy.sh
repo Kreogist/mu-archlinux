e6dee3c6cab05a32bb7bd4befcaa4fccddd7a805
frantic1048 <archer@frantic1048.com>
2016-04-04 19:18:37 +0800

    fix yml syntax err

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,12 +2,12 @@ export PKGNAME=kreogist-mu
 export PKGVER=$(<PKGBUILD grep pkgver= | cut -d"=" -f 2 | sed "s/[\"']//g")
 export PKGREL=$(<PKGBUILD grep pkgrel= | cut -d"=" -f 2 | sed "s/[\"']//g")
 
-git config --global user.email $GIT_EMAIL
-git config --global user.name $GIT_NAME
+git config --global user.email ${GIT_EMAIL}
+git config --global user.name ${GIT_NAME}
 git config --global push.default simple
 git config --global push.followTags true
 git add .
-git commit -m "Torabisu: updated kreogist-mu PKGBUILD to ${PKGVER}:${PKGREL}"
+git commit -m "Torabisu: updated ${PKGNAME} PKGBUILD to ${PKGVER}:${PKGREL}"
 git tag "$PKGVER.$PKGREL"
 git branch
 git tag
